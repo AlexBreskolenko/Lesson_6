7d44675b4bff0eebe4c96f0a66a65de4b1ffc328
Alex <abreskolenko@yandex.ru>
2023-03-02 00:08:22 +0300

    Create work lesson 6

---
+++ work/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+//Стиль Pascal
+int BackColor = 0;
+
+//Стиль Camel
+string bordColor = String.Empty;
+
+//Переменные стиль Camel методы стиль Pascale
+
+//TODO: пишем временный код
+
+
+
+bool Method(int number)
+{
+    return number % 2 == 0;
+}
+//Можно записать так 
+bool Method_2 (int number) => number % 2 == 0;
+
+//Или так 
+var Method_3 = (int number) => number % 2 == 0;
+
+//var - автоматически определяет тип
+var str = "Hello";
+var num = 4;
+
+//Узнаем тип переиенных с помощью GetType().Name
+Console.WriteLine(str.GetType().Name);
+Console.WriteLine(num.GetType().Name);
